34295495c08ef3affcbabdd281a5e33b7d937c64
grechka91 <levor23@mail.ru>
2017-10-13 14:36:02 +0300

    Update update_list_of_tempest_in_polarion.py

--- update_list_of_tempest_in_polarion.py
+++ update_list_of_tempest_in_polarion.py
@@ -168,6 +168,7 @@ def check_tempest_test_in_polarion(tempest_list, assignee, path):
                 
 def get_url_to_file_by_tempest_path(tempest_path):
     querry_name = tempest_path.rsplit('.',1)[1]
+    requests.packages.urllib3.disable_warnings()
     r = requests.get('https://api.github.com/search/code?q={}+repo:openstack/tempest'.format(querry_name), verify=False)
     return json.loads(r.content)['items'][0]['html_url']
 

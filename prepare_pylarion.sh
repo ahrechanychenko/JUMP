cd2f809a955516b1ebc3b9353e19bdd1d46d360b
root <root@seal33.qa.lab.tlv.redhat.com>
2017-10-13 07:49:32 +0000

    Initial commit for tracking

---
+++ prepare_pylarion.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Clone Pylarion library"
+rm -rf pylarion
+git clone https://code.engineering.redhat.com/gerrit/pylarion
+
+echo "Copy Pylarion config to home directory"
+rm -rf ~/.pylarion
+cp rhos-qe-core-installer/tripleo/polarion/.pylarion ~/
+
+
+echo "Setup Pylarion library"
+cd pylarion && python setup.py install && pip install --upgrade pip && pip install -r requirements.txt && cd ../
+
+
+
---
+++ update_list_of_tempest_in_polarion.py
@@ -0,0 +1,188 @@
+#!/usr/bin/python
+
+import os
+from pylarion import work_item
+import sys
+import argparse
+import subprocess
+import logging
+
+logging.getLogger('suds.client').setLevel(logging.CRITICAL)
+
+parser = argparse.ArgumentParser(description="Compare list of upstream tempest test "
+                                             "with list test cases "
+                                             "where automation-test-id == tempest test path in Polarion")
+parser.add_argument("--project_id",
+                    help="polarion project id, \n for example - RHELOpenStackPlatform", required=True)
+
+parser.add_argument("--polarion_user", default="rhos_machine", type=str,
+                    help="polarion user in short format, by default - rhosp_machine")
+
+parser.add_argument("--polarion_password", default="polarion", type=str,
+                    help="polarion user in short format, by default - polarion")
+args = parser.parse_args()
+
+# check for CA
+try:
+    os.environ["REQUESTS_CA_BUNDLE"]
+except KeyError:
+    print "Please set the environment variable REQUESTS_CA_BUNDLE"
+    sys.exit(1)
+
+# reload sys to use "utf8" instead of "unicode"
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+# get variables values
+PROJECT_ID = args.project_id
+POLARION_USER = args.polarion_user
+POLARION_PASS = args.polarion_user
+
+
+def get_tempest_test_list():
+    print "\n Install tempest \n"
+    """ 
+    1) clone upstream tempest
+    2) install in in virtual 
+    3) prepare tempest for work
+    4) run 'tempest run -l' to get list of default test
+
+    Returns:
+        list with names of tempest test
+
+    """
+    # get latest tempest repo
+    subprocess.check_call("rm -rf /tmp/tempest && git clone http://git.openstack.org/openstack/tempest /tmp/tempest",
+                          shell=True)
+    # install tempest and init configuration
+    subprocess.check_call('cd /tmp/tempest && python setup.py install && '
+                          'pip install --upgrade pip && pip install -r requirements.txt',
+                          shell=True)
+    # check if we already have tempest workspace
+    process = subprocess.Popen("tempest workspace list", shell=True, stdout=subprocess.PIPE)
+    out, err = process.communicate()
+    if len(out.split('\n')[1]) == 0:
+        subprocess.check_call('cd /tmp/tempest && tempest init cloud-test || true',
+                              shell=True)
+    subprocess.check_call('rm -rf /tmp/test_tempest_updater && mkdir /tmp/test_tempest_updater',
+                          shell=True)
+    # get list of test
+    process = subprocess.Popen("tempest run -l --config-file /tmp/tempest/cloud-test/.testr.conf",
+                               shell=True,
+                               stdout=subprocess.PIPE)
+    out, err = process.communicate()
+    tempest_test = out.split('\n')
+
+    # return list of test
+    return tempest_test[5:]
+
+
+def generate_testcase_xml_file(file_path, project_id, assignee, title, description, automation_test_id):
+    """ 
+    Generate xml for each missed test case which must exist in Polarion
+
+    Args:
+        file_path: str , path to storing xml file.
+        project_id: str, Polarion project ID.
+        assignee: str, person for assigning test case
+        title: str, title of test case
+        description: str, description of test case
+        automation_test_id: str, automation-test-id for test case 
+
+    """
+    print "\n Generate xml file for {}  \n".format(automation_test_id)
+
+    # generate content for writing to xml file
+    template = """<?xml version="1.0" ?>
+<testcases project-id="{project_id}">
+  <testcase assignee-id="{assignee}" due-date="2016-09-30" initial-estimate="">
+    <title>{title}</title>
+    <description>{description}</description>
+    <custom-fields>
+      <custom-field content="automated" id="caseautomation"/>
+      <custom-field content="{automation_test_id}" id="automation-test-id"/>
+      <custom-field content="tempest" id="automation-env"/>
+      <custom-field content="medium" id="caseimportance"/>
+      <custom-field content="Polarion-testing" id="casecomponent"/>
+      <custom-field content="component" id="caselevel"/>
+      <custom-field content="positive" id="caseposneg"/>
+      <custom-field content="yes" id="upstream"/>
+      <custom-field content="functional" id="testtype"/>
+      <custom-field content="setup" id="setup"/>
+      <custom-field content="teardown by cleaning the workspace." id="teardown"/>
+      <custom-field content="test" id="automation_script"/>
+    </custom-fields>
+  </testcase>
+</testcases>
+""".format(assignee=assignee,
+           project_id=project_id,
+           title=title,
+           description=description,
+           automation_test_id=automation_test_id)
+    # write file
+    with open('{file_path}/{name}.xml'.format(file_path=file_path, name=automation_test_id), mode="w+") as f:
+        f.write(template)
+
+
+def get_test_case_id_by_automation_id(automation_id, project):
+    """ 
+        Check if automation_id exist in polarion
+        1) Connect to Polarion and via query 
+        2) return "Not Found" or "Test Case ID"
+
+        Args:
+            automation_id: str, automation-test-id for test case
+            project: str, Polarion project ID
+
+        Returns:
+            str: "Not Found" or Polarion Test Case ID
+
+        """
+    test_case = work_item.TestCase.query(query="automation-test-id:{}".format(automation_id), project_id=project)
+    if len(test_case) == 0:
+        return "Not Found"
+    else:
+        return test_case[0].work_item_id
+
+
+def check_tempest_test_in_polarion(tempest_list, assignee, path):
+    for test in tempest_list:
+        for i in range(0, 100):
+            try:
+                res = get_test_case_id_by_automation_id(test.split("[")[0], PROJECT_ID)
+                if "Not Found" in res:
+                    generate_testcase_xml_file(file_path=path,
+                                               project_id=PROJECT_ID,
+                                               assignee=assignee,
+                                               title="tempest test which covers {}".format(test.split("[")[0]),
+                                               description="",
+                                               automation_test_id=test.split("[")[0])
+                else:
+                    print "tempest test {} exist in Polarion {} project and covered by {}".format(test.split("[")[0], PROJECT_ID, res)
+                break
+            except:
+                continue
+
+
+def update_test_cases_in_polarion(path):
+    """ 
+        Upload test cases which covering missed tempest tests via curl to stage job 
+
+
+    """
+    list_of_xml = [f for f in os.listdir(path) if
+                   os.path.isfile(os.path.join(path, f))]
+    for xml_file in list_of_xml:
+        cmd = 'curl -k -u rhosp_machine:polarion -X POST ' \
+              '-F file=@/{}/{} ' \
+              'https://polarion.engineering.redhat.com//polarion/import/testcase'.format(path, xml_file)
+        print "{} was upload to Polarion".format(xml_file)
+        subprocess.check_call(cmd,
+                              shell=True)
+
+
+if __name__ == "__main__":
+    tempest_list = get_tempest_test_list()
+    check_tempest_test_in_polarion(tempest_list, "rhosp-user", path='/tmp/test_tempest_updater')
+    #update_test_cases_in_polarion(path='/tmp/test_tempest_updater')
+

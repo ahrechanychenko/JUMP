a930f19da329c9092e5849ef90c84a31bc9b2cbc
grechka91 <levor23@mail.ru>
2017-10-13 11:53:29 +0300

    Update prepare_pylarion.sh

--- prepare_pylarion.sh
+++ prepare_pylarion.sh
@@ -9,13 +9,6 @@ rm -rf ~/.pylarion
 #cp rhos-qe-core-installer/tripleo/polarion/.pylarion ~/
 cp JUMP/.pylarion ~/
 
-
 echo "Setup Pylarion library"
-cd pylarion && python setup.py install  && cd ../
+cd pylarion && python setup.py install && pip install --upgrade pip && pip install -r requirements.txt && cd ../
 
-echo "Setup Tempest"
-# get latest tempest repo
-rm -rf /tmp/tempest && git clone http://git.openstack.org/openstack/tempest /tmp/tempest
-
-# install tempest and init configuration
-pip install tempest/

42450c56c9ebf74bf5ec0638d8576fabf72f2991
grechka91 <levor23@mail.ru>
2017-10-19 18:09:03 +0300

    Update update_automation_env_for_tempest.py

--- update_automation_env_for_tempest.py
+++ update_automation_env_for_tempest.py
@@ -86,14 +86,14 @@ def update_automation_env(test_cases):
     """
 
     for test in test_cases:
-        for i in range(0,10):
+        for i in range(0,100):
             try:
                 if test.get_custom_field('automation-env').value is None:
-                    update_automation_env(test, '001')
+                    #update_automation_env(test, '001')
                     print "test {} doesn't have automation-env".format(test.work_item_id)
                     break
                 elif test.get_custom_field('automation-env').value.id.encode() != "001":
-                    update_automation_env(test, '001')
+                    #update_automation_env(test, '001')
                     print "test {} have automation-env:{} so change it to tempest- 001".format(test.work_item_id, test.get_custom_field('automation-env').value.id.encode())
                     break
                 else:

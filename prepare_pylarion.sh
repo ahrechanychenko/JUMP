494338ce891cd8cdd65fddcf982e9ff42c0bcd68
grechka91 <levor23@mail.ru>
2018-01-09 23:15:48 +0200

    change dir
    Change dir when copy .pylarion

--- prepare_pylarion.sh
+++ prepare_pylarion.sh
@@ -6,8 +6,7 @@ git clone https://code.engineering.redhat.com/gerrit/pylarion
 
 echo "Copy Pylarion config to home directory"
 rm -rf ~/.pylarion
-#cp rhos-qe-core-installer/tripleo/polarion/.pylarion ~/
-cp JUMP/.pylarion ~/
+cp .pylarion ~/
 
 echo "Setup Pylarion library"
 cd pylarion && python setup.py install && pip install --upgrade pip && pip install -r requirements.txt && cd ../
